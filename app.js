a42de782f3164b2a015a0127b73930c3e2744ff6
noam-ackerman <noamac1812@gmail.com>
2021-06-11 12:28:53 +0200

    creating the HTML and CSS , and adding to dos function
    creating the HTML designing with CSS , and adding to dos function

---
+++ app.js
@@ -0,0 +1,36 @@
+//SELECTORS
+
+let todoInput = document.querySelector(".todo-input");
+let todoButton = document.querySelector(".todo-button");
+let todoList = document.querySelector(".todo-list");
+let filterOption = document.querySelector(".filter-todo");
+
+//events listeners
+
+//document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+
+//functions
+
+function addTodo(e) {
+  e.preventDefault();
+  let todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  let newTodo = document.createElement("li");
+  newTodo.innerHTML = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  todoInput.value = "";
+  //Create Completed Button
+  let completedButton = document.createElement("button");
+  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Create trash button
+  let trashButton = document.createElement("button");
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //attach final Todo
+  todoList.appendChild(todoDiv);
+}
